b5513b8b3a536e0cbdf0e540bed0831796f014f6
jc725951 <aaishwinderkaur@my.jcu.edu.au>
2020-05-13 16:45:21 +1000

    "Fix guitars list empty check and add comments"

---
+++ prac_6/guitars.py
@@ -0,0 +1,36 @@
+from guitar import Guitar
+
+
+def main():
+
+    guitar_list = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar_added = Guitar(name, year, cost)
+        guitar_list.append(guitar_added)
+        print(guitar_added,  "added.")
+        name = input("Name: ")
+
+    guitar_list.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitar_list.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
+
+    if guitar_list:
+
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitar_list):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                                      vintage_string))
+
+    else:
+        print("Go and buy one guitar")
+
+
+main()
